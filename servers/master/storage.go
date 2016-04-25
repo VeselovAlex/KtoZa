3a1b3a948965cb2a8356c55112b51326f7673a01
Александр Веселов < >
2016-04-25 21:40:56 +0300

    byval error fixed

--- servers/master/storage.go
+++ servers/master/storage.go
@@ -9,21 +9,21 @@ type pollStorage struct {
 	poll *core.Poll
 }
 
-func (s pollStorage) Create(poll *core.Poll) error {
+func (s *pollStorage) Create(poll *core.Poll) error {
 	s.poll = poll
 	return nil
 }
 
-func (s pollStorage) Get() (*core.Poll, error) {
+func (s *pollStorage) Get() (*core.Poll, error) {
 	return s.poll, nil
 }
 
-func (s pollStorage) Update(poll *core.Poll) error {
+func (s *pollStorage) Update(poll *core.Poll) error {
 	s.poll = poll
 	return nil
 }
 
-func (s pollStorage) Delete(poll *core.Poll) error {
+func (s *pollStorage) Delete(poll *core.Poll) error {
 	s.poll = nil
 	return nil
 }
