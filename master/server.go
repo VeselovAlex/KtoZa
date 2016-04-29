3e672151d1246484b0e9dafbc513cf4813ac39b1
Александр Веселов < >
2016-04-29 19:42:17 +0300

    dummy master server

--- master/server.go
+++ master/server.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+var addr string = ":8888"
 
 func main() {
-	fmt.Println("Hello")
+	pollCtrl := &PollController{}
+	http.Handle("/api/poll", pollCtrl)
+
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		log.Fatalf("Unable to start master server on %s: %v\n", Addr, err)
+	}
 }
